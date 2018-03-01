ac72b90a32512827d7e760ad06ff11ece81ad1d3
amankumar99 <34052634+amankumar99@users.noreply.github.com>
2018-03-01 22:13:20 +0530

    Commit_2
    priority=1+waiting time/burst time

--- commit1.c
+++ commit1.c
@@ -1,38 +1,19 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<conio.h>
-
-struct Process
+void sort1(struct Process *t,int p)
 {
-
-	int pid,at,bt;
-};
-void swap1(double *xp,double *yp)
-{
-	double temp = *xp;
-	*xp = *yp;
-	*yp = temp;
-}
-
-void swap(int *xp, int *yp)
-{
-	int temp = *xp;
-	*xp = *yp;
-	*yp = temp;
-}
-
-void selectionSort(double arr[],int arr1[] ,int n)
-{
-	int i, j;
-	int min_idx;
-	for (i = 0; i < n-1; i++) 
+	int i,j;
+	struct Process *q,s;
+	for(i=0;i<p;i++,t++) 
 	{
-		min_idx = i;
-		for (j = i+1; j < n; j++) 
-		if (arr[j] < arr[min_idx])
-		min_idx = j;
-		swap1(&arr[min_idx], &arr[i]);
-		swap(&arr1[min_idx], &arr1[i]);
+		for(j=i+1,q=t+1;j<p;j++,q++) 
+		{
+			if((t->at)>(q->at))
+				{
+					s=*t;
+					*t=*q;
+					*q=s;
+				}
+		}
 	}
 }
+
 
